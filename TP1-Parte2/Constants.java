609b4b993a112799304d8ba5074bb8d456542a5c
Alevaiin <alvain.dev@gmail.com>
2025-05-13 17:40:51 -0300

    Muevo constantes a archivo separado

---
+++ TP1-Parte2/Constants.java
@@ -0,0 +1,14 @@
+public class Constants {
+
+    private Constants(){
+
+    }
+
+    public static final int MIN_MATCH_DURATION = 100; //Duracion minima de un partido en ms
+    public static final int MAX_MATCH_DURATION = 500; //Duracion maxima de un partido en ms
+
+    public static final int MS_BETWEEN_MATCH_TICKS = 5;
+
+    public static final double PERCENTAGE_DIFFERENCE_TO_GOAL = 0.2;
+
+}
--- TP1-Parte2/Match.java
+++ TP1-Parte2/Match.java
@@ -5,13 +5,6 @@ public class Match implements Runnable {
     private final Team away;
     private MatchResult matchResult;
 
-    private final int MIN_MATCH_DURATION = 100; //Duracion minima de un partido en ms
-    private final int MAX_MATCH_DURATION = 500; //Duracion maxima de un partido en ms
-
-    private final int MS_BETWEEN_TICKS = 5;
-
-    private final double PERCENTAGE_DIFFERENCE_TO_GOAL = 0.2;
-
     public Match(Team home, Team away){
         this.home = home;
         this.away = away;
@@ -34,19 +27,19 @@ public class Match implements Runnable {
 
         Random rand = new Random();
         long startTime = System.currentTimeMillis();
-        long duration = rand.nextInt(MAX_MATCH_DURATION-MIN_MATCH_DURATION) + MIN_MATCH_DURATION;
+        long duration = rand.nextInt(Constants.MAX_MATCH_DURATION-Constants.MIN_MATCH_DURATION) + Constants.MIN_MATCH_DURATION;
 
         while(System.currentTimeMillis() < (startTime +duration)){ //Tick
             pointsHome = (int) Math.round(rand.nextGaussian() * powerHome);
             pointsAway = (int) Math.round(rand.nextGaussian() * powerAway);
 
-            if(pointsAway * PERCENTAGE_DIFFERENCE_TO_GOAL > Math.abs(pointsHome - pointsAway) )
+            if(pointsAway * Constants.PERCENTAGE_DIFFERENCE_TO_GOAL > Math.abs(pointsHome - pointsAway) )
                 if(pointsAway > pointsHome)
                     goalsAway++;
                 else
                     goalsHome++;
             try{
-                Thread.sleep(MS_BETWEEN_TICKS);
+                Thread.sleep(Constants.MS_BETWEEN_MATCH_TICKS);
             }catch (InterruptedException e){
                 System.out.println("Error en partido");
             }
